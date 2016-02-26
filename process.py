2011328c2ab7bc8e268f76845181f061bc2dba31
starcolon <tao@starcolon.com>
2016-02-26 20:47:58 +0700

    {PROCESS} Execute tokenizer as a background service upon startup

--- process.py
+++ process.py
@@ -7,11 +7,21 @@ from pydb import couch
 from pprint import pprint
 from termcolor import colored
 from pypipe import pipe as Pipe
+import subprocess
 import json
+
+def execute_background_services(commands):
+	workers = []
+	for cmd in commands:
+		print(colored('🚀 Executing...','green') + cmd)
+		sp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+		workers.append(sp)
+	return workers
 
 # Push the record through the processing pipeline
 def push_pipeline(pipe):
 	def go(rec):
+		print(rec['title'])
 		pass # TAOTODO: Trigger the pipe
 	return go
 
@@ -22,9 +32,17 @@ if __name__ == '__main__':
 	# Prepare the database server connection
 	db = couch.connector('pantip')
 
+	# Execute list of required background services
+	services = ['ruby tokenizer/tokenizer.rb']
+	workers  = execute_background_services(services)
+
 	# Prepare the processing pipeline
 	# TAOTODO:
 	pipe = []
 
 	# Iterate through each record and process
-	couch.each_do(db,make_data_row(pipe))
+	couch.each_do(db,push_pipeline(pipe))
+
+	# End of the process, terminate all background services
+	print(colored('Ending workers...','cyan'))
+	[w.kill() for w in workers]

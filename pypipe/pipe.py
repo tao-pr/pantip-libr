d1504159092cd56c0c71071770ecac575e96c94c
starcolon <tao@starcolon.com>
2016-02-26 21:17:10 +0700

    {PIPE} Drafting pipe prototype

--- pypipe/pipe.py
+++ pypipe/pipe.py
@@ -5,12 +5,31 @@ Generic record processing pipeline.
 """
 
 import pyspark
+from functools import reduce
+
+class Pipe:
+	def __init__(self,title,tasks):
+		self.title = title
+		self.tasks = tasks[:]
+
 
 # Create a task pipeline
-def make(tasks):
-	pass
+def make(title,tasks):
+	return Pipe(title,tasks)
 
 # Execute the pipeline and take
-def operate(pipe,input,callback):
-	pass
+def operate(pipe,input0,callback):
+	if pipe is None:
+		raise 'Pipe is not available'
+	# NOTE: null is allowed as input
+
+	# Sequential task runner
+	def take(a,task):
+		return task(a)
+
+	out = reduce(take,pipe.tasks,input0)
+
+	# Call the callback function
+	callback(out) if callback is not None
+
 

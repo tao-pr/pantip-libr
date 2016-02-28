4337ee0f530a17ccee9d58a0f8645cbc2e8789f5
starcolon <tao@starcolon.com>
2016-02-29 01:35:06 +0700

    {OPERATIONS} Update Hasher with dim reduction and pipeline

--- pypipe/operations/hasher.py
+++ pypipe/operations/hasher.py
@@ -4,9 +4,11 @@ Text hashing vectoriser module
 """
 
 import numpy as np
+import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.decomposition import PCA
 from sklearn.linear_model import RidgeClassifier
 from sklearn.pipeline import make_pipeline
 
@@ -18,7 +20,27 @@ def new():
 		non_negative=True,
 		binary=False)
 	idf = TfidfVectorizer()
-	return make_pipeline(hasher,idf)
 
-def vectorize(vectorizer,tokens):
-	return vectorizer.fit_transform(tokens)
+	# Prepare dimentionality reducer
+	pca = PCA(n_components=64)
+
+	# Prepare two principal processes
+	transformer = make_pipeline(hasher,idf,pca)
+	return transformer
+
+def save(transformer,path):
+	with open(path,'wb+') as f:
+		pickle.dump(transformer)
+
+def load(path):
+	with open(path,'rb') as f:
+		return pickle.load(f)
+
+# Train the vectorizer with the collection (iterable) of text data
+def explore(transformer,collection):
+	transformer.fit(collection)
+	return transformer
+
+# @return {Matrix} Term document matrix
+def vectorize(transformer,tokens):
+	return transformer.fit_transform(tokens)

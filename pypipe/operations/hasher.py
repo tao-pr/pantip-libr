385093b851c9ddaa73d397b9ea6d5cde9a2fab1d
starcolon <tao@starcolon.com>
2016-02-29 00:14:25 +0700

    {OPERATIONS} Make process pipeline

--- pypipe/operations/hasher.py
+++ pypipe/operations/hasher.py
@@ -10,13 +10,15 @@ from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.linear_model import RidgeClassifier
 from sklearn.pipeline import make_pipeline
 
+# Create a text process pipeline (vectorizer)
 def new():
 	# Prepare vectoriser engines
 	hasher = HashingVectorizer(
 		n_features=512,
 		non_negative=True,
 		binary=False)
-	return hasher
+	idf = TfidfVectorizer()
+	return make_pipeline(hasher,idf)
 
-def vectorize(hasher,tokens):
-	return hasher.fit_transform(tokens)
+def vectorize(vectorizer,tokens):
+	return vectorizer.fit_transform(tokens)

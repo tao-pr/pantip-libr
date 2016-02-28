a6828d486b7fa3afb2e443c9920afae74973bbac
starcolon <tao@starcolon.com>
2016-02-28 17:38:10 +0700

    {RABBIT} FIX: routing key

--- pypipe/operations/rabbit.py
+++ pypipe/operations/rabbit.py
@@ -11,21 +11,21 @@ def create(server_addr,q):
 	conn = pika.BlockingConnection(pika.ConnectionParameters(server_addr))
 	channel = conn.channel()
 	channel.queue_declare(queue=q)
-	return (conn,channel)
+	return (conn,channel,q)
 
 def feed(feeder):
 	def feed_message(record):
-		conn,channel = feeder
-		key  = record['topic_id']
+		conn,channel,q = feeder
+		topic_id  = record['topic_id']
 		data = json.dumps(record,ensure_ascii=False)
 		channel.basic_publish(
 			exchange='',
-			routing_key=str(key),
+			routing_key=q,
 			body=data)
-		print(colored('record #{0} fed to rabbit'.format(key),'blue'))
+		print(colored('record #{0} fed to rabbit'.format(topic_id),'blue'))
 
 	return feed_message
 
 def end(feeder):
-	conn,channel = feeder
+	conn,channel,q = feeder
 	conn.close()

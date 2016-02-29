6661c6f5cf3a02e55bb4734d69d1bb78afc7cdee
starcolon <tao@starcolon.com>
2016-02-29 23:18:15 +0700

    {RABBIT} Draft @Feeder class

--- pypipe/operations/rabbit.py
+++ pypipe/operations/rabbit.py
@@ -7,16 +7,30 @@ from termcolor import colored
 import pika
 import json
 
+class Feeder(object):
+	def __init__(self,conn,channel,q):
+		self.conn    = conn
+		self.channel = channel
+		self.q       = q
+
+	def get_params(self):
+		return (self.conn,self.channel,self.q)
+
+	def __iter__(self):
+		#TAOTODO: Pull the next message out of the queue
+		pass
+
 def create(server_addr,q):
 	conn = pika.BlockingConnection(pika.ConnectionParameters(server_addr))
 	channel = conn.channel()
 	channel.queue_declare(queue=q)
-	return (conn,channel,q)
+	feeder = Feeder(conn,channel,q)
+	return feeder
 
 # @return {Record} it remains unchanged 
 def feed(feeder):
 	def feed_message(record):
-		conn,channel,q = feeder
+		conn,channel,q = feeder.get_params()
 		topic_id  = record['topic_id']
 		data = json.dumps(record,ensure_ascii=False)
 		channel.basic_publish(
@@ -27,16 +41,8 @@ def feed(feeder):
 		return record
 	return feed_message
 
-def pull(feeder):
-	def __callback(ch, method, properties, body):
-		pass #TAOTODO:
-	conn,channel,q = feeder
-	channel.basic_consume(__callback,q,no_ack=True)
-
 def end(feeder):
-	conn,channel,q = feeder
+	conn,channel,q = feeder.get_params()
 	conn.close()
 
-# TAOTODO: Make an iterable over the MQ feeder
-def make_iterable(feeder):
-	pass
+

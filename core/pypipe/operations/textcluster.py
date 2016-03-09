9f8a1c8e456ab9021a8c2145c4acee5646a77e82
starcolon <tao@starcolon.com>
2016-03-10 00:27:01 +0700

    {TEXTCLUSTER} Initial commit

---
+++ core/pypipe/operations/textcluster.py
@@ -0,0 +1,36 @@
+"""
+Unsupervised topic classification
+@starcolon projects
+"""
+
+import pickle
+from sklearn.cluster import KMeans
+
+def new(n_labels=10):
+	kmeans = KMeans(
+		n_clusters = n_labels,
+		n_jobs = 2
+	)
+
+	return kmeans
+
+def save(operations,path):
+	with open(path,'wb+') as f:
+		pickle.dump(operations,f)
+
+def load(path):
+	with open(path,'rb') as f:
+		return pickle.load(f)
+
+# Load the hasher pipeline object
+# from the physical file,
+# or initialise a new object if the file doesn't exist
+def safe_load(path):
+	if os.path.isfile(path): return load(path)
+	else: return new()
+
+def classify(clf,learn=True):
+	def _do(matrix):
+		if learn: return clf.fit_predict(matrix)
+		else: return clf.predict(matrix)
+	return _do

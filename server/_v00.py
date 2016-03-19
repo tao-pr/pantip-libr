8b2e59300ca3fa329572a29b280b8fe241453a93
starcolon <tao@starcolon.com>
2016-03-20 01:54:42 +0700

    Add @start_server

--- server/_v00.py
+++ server/_v00.py
@@ -7,4 +7,11 @@ Service provider v00
 def process(topic):
 	# Push topic to the input MQ
 	# TAOTODO:
+	if not __is_valid(topic):
+		return '{"error":true,"reason":"Invalid request structure"}'
 	pass
+
+def __is_valid(topic):
+	if not ('title' in topic or 'topic' in topic):
+		return False
+	return True

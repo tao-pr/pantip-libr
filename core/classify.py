0a78e436378a734aff2db74b1b9a766d53cc55e5
starcolon <tao@starcolon.com>
2016-09-03 13:13:59 +0700

    [Classify] Draft Classifier class

--- core/classify.py
+++ core/classify.py
@@ -21,12 +21,35 @@ from pypipe.operations import textcluster
 
 class Classifier:
 
+  REPO_DIR = os.getenv('PANTIPLIBR','../..')
+  TEXT_VECTORIZER_PATH  = '{0}/data/models/vectoriser'.format(REPO_DIR)
+  TAG_HASHER_PATH       = '{0}/data/models/taghash'.format(REPO_DIR)
+  CLF_PATH              = '{0}/data/models/clf'.format(REPO_DIR)
+  STOPWORDS_PATH        = '{0}/data/words/stopwords.txt'.format(REPO_DIR)
+  CSV_REPORT_PATH       = '{0}/data/report.csv'.format(REPO_DIR)
+
+
   def __init__(self):
-    # TAOTODO: Load models
-    pass
+    # Load the trained models
+    self.topicHasher = texthasher.safe_load(TEXT_VECTORIZER_PATH,stop_words=[])
+    self.tagHasher   = taghasher.safe_load(TAG_HASHER_PATH,n_feature=256)
+    self.contentClf  = textcluster.safe_load(CONTENT_CLUSTER_PATH,n_labels=16)
+    self.clf         = cluster.safe_load(CLF_PATH)
 
   def classify(self,topic):
-    pass
+    # Prepare processing functions
+    hashMe     = texthasher.hash(topicHasher,learn=False)
+    clusterMe  = textcluster.classify(contentClf,learn=False)
+    hashtagMe  = taghasher.hash(tagHasher,learn=False)
+    classifyMe = cluster.analyze(clf)
+
+    v = hashMe(str(topic['title'] + topic['topic']))
+    t = ' '.join([tag for tag in topic['tags'] if len(tag)>1])
+    w = hashtagMe(t)
+    x = list(v) + list(w)
+
+    y = classifyMe(x)
+    return y
 
   def create_topic(self,title,tags,content):
     pass
